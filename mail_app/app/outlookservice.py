1bb3ed098b4ba4bf89360bfac64169f708e45b43
danson.mwema <danson.mwema@turnkeyafrica.com>
2018-08-31 15:42:42 +0300

    add Oauth2  token

--- mail_app/app/outlookservice.py
+++ mail_app/app/outlookservice.py
@@ -17,25 +17,20 @@ def make_api_call(method, url, token, payload = NONE, parameters =NONE):
   #Use these headers to instrument calls. Makes it easier
   # to correlate requests and responses in case of problems
   # and is a recommended best practice.
-request_id = str(uuid.uuid4())
-  instrumentation = {'client-request-id': request_id,
-                     'return-client-request-id': 'true'}
 
-  headers.update(instrumentation)
+  
+ 
 
-  response = None
+def get_me(access_token):
+  get_me_url = graph_endpoint.format('/me')
 
-  if (method.upper() == 'GET'):
-      response = requests.get(url, headers=headers, params=parameters)
-  elif (method.upper() == 'DELETE'):
-      response = requests.delete(url, headers=headers, params=parameters)
-  elif (method.upper() == 'PATCH'):
-      headers.update({'Content-Type': 'application/json'})
-      response = requests.patch(url, headers=headers,
-                                data=json.dumps(payload), params=parameters)
-  elif (method.upper() == 'POST'):
-      headers.update({'Content-Type': 'application/json'})
-      response = requests.post(url, headers=headers,
-                               data=json.dumps(payload), params=parameters)
+  # Use OData query parameters to control the results
+  #  - Only return the displayName and mail fields
+  query_parameters = {'$select': 'displayName,mail'}
 
-  return response
+  r = make_api_call('GET', get_me_url, access_token, "", parameters = query_parameters)
+
+  if (r.status_code == requests.codes.ok):
+    return r.json()
+  else:
+    return "{0}: {1}".format(r.status_code, r.text)
--- mail_app/app/views.py
+++ mail_app/app/views.py
@@ -2,20 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.urls import reverse 
 from app.authelper import get_signin_url
+from app.outlookservice import get_me
 
 
-#create a home view 
-
+#updated home view to  
 def home(request):
   redirect_uri = request.build_absolute_uri(reverse('app:gettoken'))
   sign_in_url = get_signin_url(redirect_uri)
   return HttpResponse('<a href="' + sign_in_url + '">Click here to sign in and view your mail</a>')
+  
+#original home view 
+'''def old_home(request):
+  sign_in_url = '#'
+  context = {'sign_in_url': sign_in_url}
+  return render(request, 'app/home.html', context)
+  '''
 
-
-
-"""def gettoken(request):
+"""def gettoken1(request):
   return HttpResponse('gettoken view')
 """
-def gettoken(request):
+"""
+def gettoken2(request):
     auth_code = request.GET['code']
+    redirect_uri = request.build['']
     return HttpResponse('Authorization code: {0}'.format(auth_code))
+"""
+# Add import statement to include new function
+
+
+def gettoken(request):
+  auth_code = request.GET['code']
+  redirect_uri = request.build_absolute_uri(reverse('app:gettoken'))
+  token = get_token_from_code(auth_code, redirect_uri)
+  access_token = token['access_token']
+  user = get_me(access_token)
+
+  # Save the token in the session
+  request.session['access_token'] = access_token
+  return HttpResponse('User: {0}, Access token: {1}'.format(user['displayName'], access_token))
+
+
